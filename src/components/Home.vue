0b54b5340da5fe3334cee8e1f92ea788b2fe4661
techtic-hemant <hemant@techtic.com>
2020-09-05 18:58:54 +0530

    project setup

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <b-container class="product-details">
+      <!-- <b-col class="block__element--modifier">test</b-col> -->
+      <b-row>
+        <b-col cols="5">
+          <b-row>
+            <p>Cap Toe Sneaker</p>
+          </b-row>
+          <b-row>
+            <span><i class="fa fa-plus"></i>Add Season</span>
+            <span v-if="seen"><i class="fa fa-plus"></i>Add Label</span>
+          </b-row>
+        </b-col>
+        <b-col cols="7">
+          <span>Idea</span>
+          <span>In Development</span>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .block {
+    &__element {
+      &--modifier {
+        color: green;
+      }
+    }
+  }
+</style>
